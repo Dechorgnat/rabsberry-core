c4b7b66f9ee9a95f4d462e9b70aab5e890b11069
A-Dechorgnat <alain.dechorgnat@orange.com>
2016-01-31 10:47:07 +0100

    error management

--- plugins/clock/read_hour.py
+++ plugins/clock/read_hour.py
@@ -2,8 +2,18 @@
 
 import sys
 import datetime
-from subprocess import call
+import subprocess
+from StringIO import StringIO
 
 if __name__ == "__main__":
     hour = datetime.datetime.now().strftime('%H')
-    call (["mpg321", "resources/mp3/clock/fr/signature.mp3", "resources/mp3/clock/fr/"+hour+"/1.mp3", "resources/mp3/clock/fr/signature.mp3"])
+
+    args = ["mpg321",
+            "-q",
+            "resources/mp3/clock/fr/signature.mp3",
+            "resources/mp3/clock/fr/"+hour+"/1.mp3",
+            "resources/mp3/clock/fr/signature.mp3"]
+    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = p.communicate()
+    if p.returncode != 0:
+        raise subprocess.CalledProcessError(p.returncode, output, error)
