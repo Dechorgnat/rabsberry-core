41b8fccce7b17ea44d7735a2d22693983d6c388a
Dechorgnat <dechorgnata@gmail.com>
2016-02-01 07:37:53 +0100

    introducing config file and core functions

--- plugins/clock/read_hour.py
+++ plugins/clock/read_hour.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python
 
+import os
 import sys
 import datetime
 import subprocess
 from StringIO import StringIO
+import json
+
+def getScriptPath():
+    return os.path.dirname(os.path.realpath(sys.argv[0]))
+
+
+with open(getScriptPath()+'/../../config.json') as json_data_file:
+    conf = json.load(json_data_file)
+#print(conf)
+
 
 if __name__ == "__main__":
     hour = datetime.datetime.now().strftime('%H')
 
     args = ["mpg321",
             "-q",
-            "resources/mp3/clock/fr/signature.mp3",
-            "resources/mp3/clock/fr/"+hour+"/1.mp3",
-            "resources/mp3/clock/fr/signature.mp3"]
+            conf['CORE_ROOT']+"/resources/mp3/clock/fr/signature.mp3",
+            conf['CORE_ROOT']+"/resources/mp3/clock/fr/"+hour+"/1.mp3",
+            conf['CORE_ROOT']+"/resources/mp3/clock/fr/signature.mp3"]
     p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output, error = p.communicate()
     if p.returncode != 0:
