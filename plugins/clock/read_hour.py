80eeae2bbf1d1df33ec132fc1a29aa8fa2862de9
A-Dechorgnat <alain.dechorgnat@orange.com>
2016-02-02 18:48:55 +0100

    use play_audio and randomize hour mp3 file

--- plugins/clock/read_hour.py
+++ plugins/clock/read_hour.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python
 
 import datetime
-import subprocess
+import random
+import os
 
 from core.tools.config import getConfig
-conf = getConfig()
-#print(conf)
+from core.functions.play_audio import play_audio_files
 
+conf = getConfig()
+print(conf)
 
 if __name__ == "__main__":
     hour = datetime.datetime.now().strftime('%H').lstrip("0")
-    args = ["mpg321",
-            "-q",
-            conf['CORE_ROOT']+"/resources/mp3/clock/fr/signature.mp3",
-            conf['CORE_ROOT']+"/resources/mp3/clock/fr/"+hour+"/1.mp3",
-            conf['CORE_ROOT']+"/resources/mp3/clock/fr/signature.mp3"]
-    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = p.communicate()
-    if p.returncode != 0:
-        raise subprocess.CalledProcessError(p.returncode, output, error)
+
+    files = os.listdir( conf['CORE_ROOT'] + "/resources/mp3/clock/fr/" + hour )
+    random.shuffle(files)
+    print files[0]
+    play_audio_files([
+        conf['CORE_ROOT'] + "/resources/mp3/clock/fr/signature.mp3",
+        conf['CORE_ROOT'] + "/resources/mp3/clock/fr/" + hour + "/"+files[0],
+        conf['CORE_ROOT'] + "/resources/mp3/clock/fr/signature.mp3"])
