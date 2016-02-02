47c67457fcba3e3b11befea0d9fcba1b2501c25f
A-Dechorgnat <alain.dechorgnat@orange.com>
2016-02-02 19:25:35 +0100

    add read_weather_broadcast

---
+++ plugins/weather/read_weather_broadcast.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Nous commencons par inclure les librairies que nous avons installées.
+import urllib
+import requests
+from core.functions.play_tts import play_tts
+
+# Nous construisons ensuite la requête pour obtenir le temps actuel.
+openWeatherMapQuery = {
+    'q': 'Rennes, France',
+    'lang': 'fr',
+    'units': 'metric',
+    'appid': '0885ba357698e289ada592480c0cb75e'
+}
+openWeatherMapURL = "http://api.openweathermap.org/data/2.5/weather?"+ urllib.urlencode(openWeatherMapQuery)
+
+# Nous procédons ensuite à la requête de l'API.
+r = requests.get(openWeatherMapURL)
+if r.status_code >= 400 :
+    print "Failed to call openWeatherMapURL on ",openWeatherMapURL
+    exit(1)
+
+resp=r.json()
+print resp
+
+# Nous construisons le texte à réciter.
+text = u"Bulletin météo : " + resp['weather'][0]['description']
+
+# Ensuite, nous ajoutons la température pour conclure la phrase. Notez, que nous l'arrondissons.
+text = text + u". Il fait actuellement "+ str(resp['main']['temp']).replace('.',',') +u" degrés."
+
+print text
+
+play_tts(text.encode('utf8'), 'fr')
