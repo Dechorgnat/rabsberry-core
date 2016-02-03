51444327133d69cf3d5b1f904d9f69a2f1076f3f
A-Dechorgnat <alain.dechorgnat@orange.com>
2016-02-03 14:08:46 +0100

    fix  exact hour

--- plugins/clock/read_exact_hour.py
+++ plugins/clock/read_exact_hour.py
@@ -10,6 +10,6 @@ print(conf)
 
 if __name__ == "__main__":
     hour = datetime.datetime.now().strftime('%H').lstrip("0")
-    minute = datetime.datetime.now().strftime('%m').lstrip("0")
+    minute = datetime.datetime.now().strftime('%M').lstrip("0")
 
-    play_tts( str(hour)+ " heures "+ str(minute) )
+    play_tts( "Il est " + str(hour)+ " heures "+ str(minute), 'fr')
