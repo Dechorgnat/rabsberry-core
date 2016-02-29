2c0079f1e841b09222a5684ac99eb3b1022b0f00
Dechorgnat <dechorgnata@gmail.com>
2016-02-29 07:40:31 +0100

    update

--- functions/ears/ear.py
+++ functions/ears/ear.py
@@ -26,3 +26,38 @@ class Ear:
 
     def stop(self):
         GPIO.output(self.enable,GPIO.LOW)
+
+
+if __name__ == "__main__":
+    GPIO.setmode(GPIO.BOARD)
+
+    left_ear = Ear(23, 19, 21)
+    right_ear = Ear(12, 16, 18)
+
+    print "Going forwards"
+    left_ear.start(Ear.forward)
+    sleep(1.5)
+    left_ear.stop()
+
+    right_ear.start(Ear.forward)
+    sleep(1.5)
+    right_ear.stop()
+
+    left_ear.start(Ear.forward)
+    sleep(1.5)
+    left_ear.stop()
+
+    right_ear.start(Ear.forward)
+    sleep(1.5)
+    right_ear.stop()
+
+    print "Going backwards"
+    left_ear.start(Ear.backward)
+    right_ear.start(Ear.backward)
+    sleep(3)
+
+    print "Now stop"
+    left_ear.stop()
+    right_ear.stop()
+
+    GPIO.cleanup()
