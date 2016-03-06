f2212d5678b2881012accf1251affb84ca056588
Dechorgnat <dechorgnata@gmail.com>
2016-03-06 07:31:31 +0100

    calibration ok

--- functions/ears/calibrate.py
+++ functions/ears/calibrate.py
@@ -8,8 +8,9 @@ current_milli_time = lambda: int(round(time.time() * 1000))
 bouncetime = 20
 last_tick = 0
 list_delay = []
-list_delay.append(200)
+list_delay.append(250)
 num = 0
+found_missing =False
 
 # il y avait 20 dents sur la roue mais 3 ont ete enlevees
 
@@ -17,26 +18,32 @@ def front_detected(channel):
     global last_tick
     global list_delay
     global num
+    global right_ear
+    global found_missing
     delay = current_milli_time() - last_tick
     if delay>bouncetime:
         last_tick = current_milli_time()
         num = num+1
         mean = sum(list_delay) / float(len(list_delay))
         if math.fabs(delay-mean)>100:
+            num = 0
+            found_missing = True
             print "missing tooth (", num, ") ", delay, "ms / mean : ",mean, " ms"
         else:
             print "tooth (", num, ") ", delay, "ms / mean : ",mean, " ms"
             list_delay.append(delay)
     else:
         print "bounce ", delay, " ms"
+    if found_missing and num == 13:
+        right_ear.stop()
 
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
-GPIO.setup(control_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(control_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-GPIO.add_event_detect(control_pin, GPIO.FALLING, callback=front_detected)
+GPIO.add_event_detect(control_pin, GPIO.FALLING, callback=front_detected, bouncetime=20)
 
 last_tick = current_milli_time()
 
@@ -48,7 +55,7 @@ else:
 right_ear = Ear(12, 16, 18, control_pin)
 #left_ear = Ear(23, 19, 21, control_pin)
 
-#right_ear.start(Ear.forward)
+'''
 while True:
     if GPIO.input(control_pin):
         print('Input was HIGH')
@@ -58,9 +65,8 @@ while True:
 
 '''
 right_ear.start(Ear.FORWARD)
-time.sleep(6)
+time.sleep(10)
 right_ear.stop()
-'''
 
 right_ear.cleanup()
 
