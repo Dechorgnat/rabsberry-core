f97f5d9592ba1abdd680a377d741b225922a62cc
Dechorgnat <dechorgnata@gmail.com>
2016-03-19 16:35:15 +0100

    add log and fix button bounce

--- functions/top_button/top_button.py
+++ functions/top_button/top_button.py
@@ -5,6 +5,8 @@ import time
 import requests
 import signal
 import sys
+import logging
+logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.DEBUG, filename='/var/log/rabsberry/top_button.log')
  
 
 class Top_button:
@@ -17,7 +19,8 @@ class Top_button:
         GPIO.setup(self.top_button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     def start_listening(self):
-        GPIO.add_event_detect(self.top_button_pin, GPIO.FALLING, callback=self.button_pressed,  bouncetime=800) 
+        logging.info('Top Button starts listening')
+        GPIO.add_event_detect(self.top_button_pin, GPIO.BOTH, callback=self.button_pressed,  bouncetime=800) 
 
     def stop_listening(self):
         GPIO.remove_event_detect(self.top_button_pin)
@@ -26,12 +29,15 @@ class Top_button:
         GPIO.cleanup(self.top_button_pin)
 
     def button_pressed(self, channel):
-        print "button pressed "
-        self.callback_short_pressed()
+        if GPIO.input(channel):
+            logging.debug("top button released")
+        else:
+            logging.debug("top button pressed")
+            self.callback_short_pressed()
+
 
 def signal_term_handler(signal, frame):
-    print
-    print 'Terminating Top Button'
+    logging.info('Terminating Top Button')
     top_button.stop_listening()
     top_button.cleanup()
     sys.exit(0)
@@ -43,7 +49,7 @@ def call_rabsberry_event_api(action):
 
 
 def callback_short_pressed():
-    print "callback_short_pressed"
+    logging.debug("callback_short_pressed")
     call_rabsberry_event_api("SHORT_PRESSED")
 
 
