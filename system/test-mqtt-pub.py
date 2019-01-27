da5ae7a6827b92b7c98f321b4f6514f9ef23d5b0
Dechorgnat <azaz1234>
2019-01-28 00:17:06 +0100

    update test utility

--- system/test-mqtt-pub.py
+++ system/test-mqtt-pub.py
@@ -1,7 +1,15 @@
-import time
+
 import paho.mqtt.client as paho
+import argparse
 
 broker="127.0.0.1"
+
+parser=argparse.ArgumentParser(
+        description='''MQTT test utility ''',
+        epilog="""""")
+parser.add_argument('--channel', default='leds', choices=['leds', 'ears'])
+parser.add_argument('msg', nargs='+',  help='text message')
+args=parser.parse_args()
 
 client= paho.Client("controller") #create client object client1.on_publish = on_publish #assign function to callback client1.connect(broker,port) #establish connection client1.publish("house/bulb1","on")
 
@@ -9,5 +17,5 @@ client= paho.Client("controller") #create client object client1.on_publish = on_
 print("connecting to broker ",broker)
 client.connect(broker)#connect
 print("publishing ")
-client.publish("leds","on")#publish
+client.publish(args.channel, args.msg)#publish
 client.disconnect() #disconnect
