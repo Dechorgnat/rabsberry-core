9ad756a9134f57c9ab7e1c2bd17253af945c02d4
Dechorgnat <rance2001>
2019-02-01 19:23:42 +0100

    fix led_manager.py

--- functions/led/led_manager.py
+++ functions/led/led_manager.py
@@ -19,7 +19,7 @@ from colors import *
 LED_COUNT = 5  # Number of LED pixels.
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10  # DMA channel to use for generating signal (try 5)
-LED_BRIGHTNESS = 40  # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 255 # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
 
 NOSE = 0
@@ -83,30 +83,31 @@ def clearStrip(strip):
 #define MQTT callback
 def on_message(client, userdata, message):
     message =str(message.payload.decode("utf-8"))
-    print("received message =", message)
+    # print("received message =", message)
     event = json.loads(message)
-    if event.command == "stop":
+
+    if event['command'] == 'stop':
+        print event['command']
         return stop_manager()
-    if event.command == "on":
+    if event['command'] == 'on':
+        print event['command']
         func_table[0] = (waveOneColor, (4., 0, 255, 0, 0))
         func_table[1] = (waveOneColor, (4., 0, 255, 0, 0))
         func_table[2] = (waveOneColor, (4., 0, 255, 0, 0))
         return
-    if event.command == "off":
+    if event['command'] == 'off':
+        print event['command']
         func_table[0] = (waveOneColor, (3., 0, 255, 0, 0))
         func_table[1] = (waveOneColor, (3., 1.0, 255, 255, 0))
         func_table[2] = (waveOneColor, (3., 2.0, 0, 0, 255))
         return
-    if event.command == "set":
-        func_table[event.led] = (fixedColor, Color(event.r, event.g, event.b))
+    if event['command'] == 'set':
+        for i in event['leds']:
+            func_table[int(i)] = (fixedColor, Color(event['r'] , event['g'], event['b'])) 
         return
-    if event.command == "set_all":
-        for i in range(0, LED_COUNT-1):
-            func_table[i] = (fixedColor, Color(event.r, event.g, event.b))
-        return
-    if event.command == "set_all_off":
-        for i in range(0, LED_COUNT-1):
-            func_table[i] = (fixedColor, BLACK)
+    if event['command'] == 'unset':
+        for i in event['leds']:
+            func_table[int(i)] = (fixedColor, BLACK) 
         return
 
 # Main program logic follows:
@@ -138,6 +139,8 @@ if __name__ == '__main__':
     # set signal handler to catch ctrl C
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
+
+    print 'Ready to receive commands'
 
     while True:
         t = time.time()
